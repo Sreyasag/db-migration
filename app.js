4f02e04b667398d9fe85aaace07a6f5efafe2c58
Sreyas <sreyasag@gmail.com>
2023-04-20 19:05:04 +0530

    updated

--- app.js
+++ app.js
@@ -2,41 +2,41 @@ const db = require('./connection');
 const fs = require('fs');
 
 const { getSocialMedia, getCountries, getSubjects, getActivityLog, getBookings, getCampaigns, getUsers, getEvents, getCompanies } = require('./fetchData');
-const { replaceIdActivityLog, replaceIdBookings } = require('./replaceIds');
+const { replaceIdActivityLog, replaceIdBookings, replaceIdCampaigns, replaceIdUsers } = require('./replaceIds');
 
 const getCollections = async () => {
     try {
 
         //get data
-        const [socialMedia, socialMediaIdMap] = await getSocialMedia();
-        console.log('socialMedia',socialMedia, 'socialMediaIdMap', socialMediaIdMap);
+        // const [socialMedia, socialMediaIdMap] = await getSocialMedia();
+        // console.log('socialMedia',socialMedia, 'socialMediaIdMap', socialMediaIdMap);
         const [countries, countriesIdMap] = await getCountries();
-        console.log('countries', countries, 'countriesIdMap', countriesIdMap);
-        const [subjects, subjectsIdMap] = await getSubjects();
-        console.log('subjects', subjects, 'subjectsIdMap', subjectsIdMap);
-        const [activityLog, activityLogIdMap] = await getActivityLog();
-        console.log('activityLog', activityLog, 'activityLogIdMap', activityLogIdMap);
+        // console.log('countries', countries, 'countriesIdMap', countriesIdMap);
+        // const [subjects, subjectsIdMap] = await getSubjects();
+        // console.log('subjects', subjects, 'subjectsIdMap', subjectsIdMap);
+        // const [activityLog, activityLogIdMap] = await getActivityLog();
+        // console.log('activityLog', activityLog, 'activityLogIdMap', activityLogIdMap);
         const [bookings, bookingsIdMap] = await getBookings();
-        console.log('bookings', bookings, 'bookingsIdMap', bookingsIdMap);
-        const [campaigns, campaignsIdMap] = await getCampaigns();
-        console.log('campaigns', campaigns, 'campaignsIdMap', campaignsIdMap);
+        // console.log('bookings', bookings, 'bookingsIdMap', bookingsIdMap);
+        // const [campaigns, campaignsIdMap] = await getCampaigns();
+        // console.log('campaigns', campaigns, 'campaignsIdMap', campaignsIdMap);
         const [users, usersIdMap] = await getUsers();
-        console.log('users', users, 'usersIdMap', usersIdMap);
-        const [events, eventsIdMap] = await getEvents();
-        console.log('events', events, 'eventsIdMap', eventsIdMap);
+        // console.log('users', users, 'usersIdMap', usersIdMap);
+        // const [events, eventsIdMap] = await getEvents();
+        // console.log('events', events, 'eventsIdMap', eventsIdMap);
         const [companies, companiesIdMap] = await getCompanies();
-        console.log('companies', companies, 'companiesIdMap', companiesIdMap);
+        // console.log('companies', companies, 'companiesIdMap', companiesIdMap);
 
        // write to file
-       fs.writeFileSync('data/socialMedia.txt', JSON.stringify(socialMedia));
-       fs.writeFileSync('data/countries.txt', JSON.stringify(countries));
-       fs.writeFileSync('data/subjects.txt', JSON.stringify(subjects));
-       fs.writeFileSync('data/activityLog.txt', JSON.stringify(activityLog));
-       fs.writeFileSync('data/bookings.txt', JSON.stringify(bookings));
-       fs.writeFileSync('data/campaigns.txt', JSON.stringify(campaigns));
-       fs.writeFileSync('data/users.txt', JSON.stringify(users));
-       fs.writeFileSync('data/events.txt', JSON.stringify(events));
-       fs.writeFileSync('data/companies.txt', JSON.stringify(companies));
+    //    fs.writeFileSync('data/socialMedia.txt', JSON.stringify(socialMedia));
+    //    fs.writeFileSync('data/countries.txt', JSON.stringify(countries));
+    //    fs.writeFileSync('data/subjects.txt', JSON.stringify(subjects));
+    //    fs.writeFileSync('data/activityLog.txt', JSON.stringify(activityLog));
+    //    fs.writeFileSync('data/bookings.txt', JSON.stringify(bookings));
+    //    fs.writeFileSync('data/campaigns.txt', JSON.stringify(campaigns));
+    //    fs.writeFileSync('data/users.txt', JSON.stringify(users));
+    //    fs.writeFileSync('data/events.txt', JSON.stringify(events));
+    //    fs.writeFileSync('companies.txt', JSON.stringify(companies));
 
 
         //map mysqlId to mongoId 
@@ -45,7 +45,10 @@ const getCollections = async () => {
         // console.log('updatedActivityLog', updatedActivityLog);
         // const updatedBookings = replaceIdBookings(bookings, countriesIdMap, subjectsIdMap, companiesIdMap)
         // console.log('updatedBookings', updatedBookings);
-        
+        // const updatedCampaigns = replaceIdCampaigns(campaigns, socialMediaIdMap, usersIdMap, bookingsIdMap)
+        // console.log('updatedCampaigns', updatedCampaigns);
+        const updatedUsers = replaceIdUsers(users, countriesIdMap,usersIdMap, bookingsIdMap, companiesIdMap)
+        console.log('updatedUsers', updatedUsers);
 
     } catch (error) {
         console.log(error)
--- replaceIds.js
+++ replaceIds.js
@@ -21,6 +21,51 @@ const replaceIdBookings = (bookings,countryIdMap, subjectIdMap, companyIdMap) =>
     return updatedBookings;
 }
 
+const replaceIdCampaigns = (campaigns,socialIdMap,userIdMap, bookingsIdMap) => {
+    const updatedCampaigns = campaigns.map(obj => {
+        const source = obj.source != null ? socialIdMap[obj.source] : null
+        const signups =  obj.campaign_leads.signups.map(refId => userIdMap[refId]);
+        const customer_bookings =  obj.campaign_leads.customer_bookings.map(refId => bookingsIdMap[refId]);
+        const manual_bookings =  obj.campaign_leads.manual_bookings.map(refId => bookingsIdMap[refId]);      
+        const updatedObj = {
+            ...obj, 
+            source, 
+            campaign_leads:{
+                ...obj.campaign_leads,
+                signups,
+                customer_bookings,
+                manual_bookings
+            }
+         };
+        return updatedObj; 
+    });
+    return updatedCampaigns;
+}
+
+const replaceIdUsers = (users, countryIdMap, userIdMap, bookingsIdMap, companiesIdMap) => {
+    const updatedUsers = users.map(obj => {
+        const country_id = obj.country_id != null ? countryIdMap[obj.country_id] : null
+        const referred_by_user = obj.referred_by_user != null ? userIdMap[obj.referred_by_user] : null
+        const bookings =  obj.bookings.map(refId => bookingsIdMap[refId]);
+        const reviews = obj.reviews.map(review => {
+            const company_id = obj.company_id != null ? companiesIdMap[obj.company_id] : null
+            return {
+                ...review,
+                company_id
+            }
+        })
+        const updatedObj = {
+            ...obj, 
+            country_id,
+            referred_by_user,
+            bookings,
+            reviews
+        };
+        return updatedObj; 
+    });
+    return updatedUsers;
+}
+
 // const updatedMongoObjArray = mysqlObjArray.map(obj => {
 //     const updatedRefs = obj.refIds.map(refId => mysqlToMongoMap[refId]);
 //     const updatedObj = { ...obj, refIds: updatedRefs };
@@ -30,5 +75,6 @@ const replaceIdBookings = (bookings,countryIdMap, subjectIdMap, companyIdMap) =>
 module.exports = {
     replaceIdActivityLog,
     replaceIdBookings,
-    
+    replaceIdCampaigns,
+    replaceIdUsers,
 }
